697991f7bb7973c7032dea68f54f7508152d9ed8
Marah Jouda <marahjouda51@gmail.com>
2023-07-12 01:25:17 +0300

    added views

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -2,65 +2,86 @@
     <div>
         <form action="/" method="post">
             @csrf
-            <div>
-                <label for="username">UserName</label>
-                <input id="username" type="text" name="username" class="@error('username') is_invalid @enderror"/><br><br>
+            <div class="form-group row">
+                <label for="username" class="col-sm-2 col-form-label">UserName</label>
+                <div class="col-sm-10">
+                    <input id="username" type="text" name="username" 
+                    class="@error('username') is_invalid @enderror"/>
+                </div>
                 @error('username')
                     <div>{{$message}}</div>
                 @enderror
             </div>
-            <div>
-                <label for="email">User email</label>
-                <input id="email" type="text" name="email" class="@error('email') is_invalid @enderror"/><br><br>
+            <div class="form-group row">
+                <label for="email" class="col-sm-2 col-form-label">User email</label>
+                <div class="col-sm-10">
+                    <input id="email" type="text" name="email"
+                    class="@error('email') is_invalid @enderror"/>
+                </div>
                 @error('email')
                     <div>{{$message}}</div>
                 @enderror
             </div>
-            <div>
-                <label for="first_name">First Name</label>
-                <input id="first_name" type="text" name="first_name" class="@error('first_name') is_invalid @enderror"/><br><br>
+            <div class="form-group row">
+                <label for="first_name" class="col-sm-2 col-form-label">First Name</label>
+                <div class="col-sm-10">
+                    <input id="first_name" type="text" name="first_name"
+                    class="@error('first_name') is_invalid @enderror"/>
+                </div>
                 @error('first_name')
                     <div>{{$message}}</div>
                 @enderror
             </div>
-            <div>
-                <label for="last_name">Last Name</label>
-                <input id="last_name" type="text" name="last_name" class="@error('last_name') is_invalid @enderror"/><br><br>
+            <div class="form-group row">
+                <label for="last_name" class="col-sm-2 col-form-label">Last Name</label>
+                <div class="col-sm-10">
+                    <input id="last_name" type="text" name="last_name" 
+                    class="@error('last_name') is_invalid @enderror"/>
+                </div>
                 @error('last_name')
                     <div>{{$message}}</div>
                 @enderror
             </div>
-            <div>
-                <label for="is_admin">Enter 0 for user or 1 for admin</label>
-                <input id="is_admin" type="text" name="is_admin" class="@error('is_admin') is_invalid @enderror"/><br><br>
+            <div class="form-group row">
+                <label for="is_admin" class="col-sm-2 col-form-label">Enter 0 for user or 1 for admin</label>
+                <div class="col-sm-10">
+                    <input id="is_admin" type="text" name="is_admin" 
+                    class="@error('is_admin') is_invalid @enderror"/>
+                </div>
                 @error('is_admin')
                     <div>{{$message}}</div>
                 @enderror
             </div>
-            <div>
-                <label for="is_active">Enter 0 for inactive or 1 for active</label>
-                <input id="is_active" type="text" name="is_active" class="@error('is_active') is_invalid @enderror"/><br><br>
+            <div class="form-group row">
+                <label for="is_active" class="col-sm-2 col-form-label">Enter 0 for inactive or 1 for active</label>
+                <div class="col-sm-10">
+                  <input id="is_active" type="text" name="is_active" 
+                  class="@error('is_active') is_invalid @enderror"/>
+                </div>
                 @error('is_active')
                     <div>{{$message}}</div>
                 @enderror
             </div>
-            <div>
-                <label for="password">Password</label>
-                <input id="password" type="password" name="password" class="@error('password') is_invalid @enderror"/><br><br>
+            <div class="form-group row">
+                <label for="password" class="col-sm-2 col-form-label">Password</label>
+                <div class="col-sm-10">
+                    <input id="password" type="password" name="password" 
+                    class="@error('password') is_invalid @enderror"/>
+                </div>
                 @error('password')
                     <div>{{$message}}</div>
                 @enderror
             </div>
-            <div>
-                <label for="confirm_password">Confirm Password</label>
-                <input id="confirm_password" type="password" name="confirm_password" class="@error('confirm_password') is_invalid @enderror"/><br><br>
+            <div class="form-group row">
+                <label for="confirm_password" class="col-sm-2 col-form-label">Confirm Password</label>
+                <div class="col-sm-10">
+                    <input id="confirm_password" type="password" name="confirm_password" class="@error('confirm_password') is_invalid @enderror"/><br><br>
+                </div>
                 @error('confirm_password')
                     <div>{{$message}}</div>
                 @enderror
             </div>
-            
-            <input type="submit" name="create" value="Create user">
-            
+            <button type="submit" class="btn btn-primary">Create User</button>            
         </form>
     </div>
 </x-layout>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,22 +1,34 @@
 <x-layout>
-    <nav aria-label="Page navigation example">
-        <h1>Users List</h1>
-        <ul class="pagination">
+    <h1>Users List</h1>
+    <div class="d-flex flex-column">     
+        <ul class="list-group">
         @foreach($users as $user)
-        <span><li class="page-item">{{$user->username}}  -  {{ $user->email }}
+        <div class="d-flex flex-row ">
+        <li class="list-group-item nav-item ">{{$user->username}}   -   {{ $user->email }}
             @if($user->is_active==1)
-            -  {{ $user->email }}
+            -   Active
+         
+            @else
+            -   Inactive
             @endif
-            <br><a href="/users/{{$user->id}}/edit">Edit</a> 
+            @if($user->is_admin==1)
+            -   Admin
+         
+            @else
+            -   User
+            @endif
+        </div>
+        <div class="d-flex flex-row-reverse">
+            <br><a class="btn btn-outline-info" href="/users/{{$user->id}}/edit">Edit</a> 
             <form action="/users/{{$user->id}}" method="post">
                 @csrf
                 @method('DELETE')
-                <button>Delete</button>
+                <button class="btn btn-outline-danger ">Delete</button>
             </form>
-       
-        </li></span>
+        </div>
+        </li>
         @endforeach
         </ul>
-    </nav>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </x-layout>
